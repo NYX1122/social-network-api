0496435082b149b7734dad50f5ccc70837ea5ca1
BoDee <nyx0hemir@gmail.com>
2021-09-03 23:39:53 -0600

    finished initializing user model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username missing',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email missing',
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill out a valid email address']
+        },
+        // thoughts: [
+        //     {
+        //         type: Schema.Types.ObjectId,
+        //         ref: 'Thought'
+        //     }
+        // ],
+        // friends: [
+        //     {
+        //         type: Schema.Types.ObjectId,
+        //         ref: 'Friend'
+        //     }
+        // ]
+    },
+    {
+        id: false
+    }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = { User };
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(require('./routes'));
+
+mongoose.connect('mongodb://localhost/social-network-api');
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
